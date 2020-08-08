b5f3a6237e60ae541ab80d0c2771a40a1c40d7c0
YuYanYan123 <68427478+YuYanYan123@users.noreply.github.com>
2020-08-08 16:31:17 +0800

    修改登录模块

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import '@/styles/index.scss' // global css
-import store from './store'
 import '@/icons' // icon
 import '@/permission' // permission control
-
+import  '@/assets/css/reset.css'
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
